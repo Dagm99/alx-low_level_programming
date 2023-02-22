7b193d4c9556d21d8d4d01c9c53fd40ee294a6ad
Dagm99 <dagmsol99@outlook.com>
2023-02-22 10:36:10 -0800

    working?

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char out[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(out[i]);
+
+	}
+	_putchar('\n');
+
+	return (0);
+
+}
