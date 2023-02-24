ae7884186b775d2081246a10a5c93a01301755d2
Dagm99 <dagmsol99@outlook.com>
2023-02-23 18:30:16 -0800

    10 debug

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,12 +8,15 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 1; i <= size; i++)
+	if (n > 0)
 	{
-		for (j = size - i; j > 0; j--)
-			_putchar(' ');
-		for (k = i; k > 0; k--)
-			_putchar('#');
-		_putchar('\n');
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+				_putchar(' ');
+			for (k = i; k > 0; k--)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
