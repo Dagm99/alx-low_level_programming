f6e74ad13a80a0c35568c5bcf92441c8a4681991
Dagm99 <dagmsol99@outlook.com>
2023-02-23 17:36:38 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - for the FizzBuzz challenge
+ *
+ * Return: 0 siccessful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
